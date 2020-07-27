ffe224f643f32408213710f823290edfbe590a67
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2020-07-27 20:51:54 +0200

    changed description and removed private network ip address to make it work out of the box

--- README.md
+++ README.md
@@ -10,6 +10,6 @@
 - /var/www/html: TSI webpage files needs to be placed in here
 
 # run it
-> docker build -t test-tsi -f Dockerfile.TSI .
+> docker build -t test-tsi -f Dockerfile .
 >
 > docker run -it --rm --name testtsi -p 80:80 -v /root/tsi/web/:/var/www/html/ -e TZ=Europe/Berlin test-tsi:latest bash
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t test-tsi -f Dockerfile .
-docker run -it --rm --name testtsi --network="internal" --ip=172.20.18.18 -p 80:80 -v /root/tsi/web/:/var/www/html/ -e TZ=Europe/Berlin test-tsi:latest bash
+docker run -it --rm --name testtsi -p 80:80 -v /root/tsi/web/:/var/www/html/ -e TZ=Europe/Berlin test-tsi:latest bash
